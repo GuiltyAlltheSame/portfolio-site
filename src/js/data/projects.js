4330be7d60837ed40a6f875822716eef1cd4180c
GuiltyAlltheSame <117597178+GuiltyAlltheSame@users.noreply.github.com>
2026-04-27 01:31:14 -0700

    js logic and folder rebuild

---
+++ src/js/data/projects.js
@@ -0,0 +1,33 @@
+export const demoProjects = [
+  {
+    title: 'Flexboxer.com',
+    line: '"CRT" style portfolio landing',
+    img: 'assets/img/card_retro_portfolio.png',
+    git: 'https://github.com/GuiltyAlltheSame/portfolio-site',
+    link: 'https://flexboxer.com/'
+  },
+  {
+    title: 'Spiza.com',
+    line: 'Knited toys shop',
+    img: 'assets/img/question.png',
+    hover: 'assets/img/white-noise.gif',
+    git: 'https://github.com/you/react-blog',
+    link: 'https://blog.you.com'
+  },
+  {
+    title: 'Cinemorph',
+    line: 'Cinema prod portfolio landing',
+    img: 'assets/img/question.png',
+    hover: 'assets/img/white-noise.gif',
+    git: 'https://github.com/GuiltyAlltheSame/cinemorph-site',
+    link: '#'
+  },
+  {
+    title: '???',
+    line: '???',
+    img: 'assets/img/question.png',
+    hover: 'assets/img/white-noise.gif',
+    git: 'https://github.com/you/node-api',
+    link: '#'
+  }
+];
